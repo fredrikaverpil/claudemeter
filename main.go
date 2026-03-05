2d9b789529872bdd2bc10ccfe89c0c23731cb190
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-03-05 10:45:03 +0100

    feat: render cwd name in status line output

--- main.go
+++ main.go
@@ -232,6 +232,11 @@ func run(cfg config) error {
 	// Render output.
 	sep := dim + " │ " + ansiReset
 	output := identity
+	if cfg.showCwd {
+		if name := cwdName(data.Cwd, cfg.cwdMaxLen); name != "" {
+			output += sep + dim + name + ansiReset
+		}
+	}
 	if cfg.showGitBranch {
 		if branch := compactName(getBranch(), cfg.gitBranchMaxLen); branch != "" {
 			output += sep + dim + branch + ansiReset
